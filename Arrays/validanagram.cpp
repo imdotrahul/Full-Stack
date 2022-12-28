f364ce9715634540da1d373f80851fcd5d457544
Rahul Yadav <imdotrahul@gmail.com>
2022-12-28 23:54:30 +0530

    validAnagram

---
+++ Arrays/validanagram.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+bool validanagram(std::string s, std::string t)
+{
+    
+}
+
+int main()
+{
+    int x;
+    cin>>x;
+    while(x--)
+    {
+        std::string s,t;
+        std:: cout<<validanagram(s,t);
+    }
+}
