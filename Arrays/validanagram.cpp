1cd50b44d74634586a42f00f9abc49f52f528392
Rahul Yadav <imdotrahul@gmail.com>
2023-02-07 15:36:22 +0530

    long time

--- Arrays/validanagram.cpp
+++ Arrays/validanagram.cpp
@@ -1,17 +1,9 @@
 #include<iostream>
 
-bool validanagram(std::string s, std::string t)
-{
-    
-}
 
 int main()
 {
     int x;
-    cin>>x;
-    while(x--)
-    {
-        std::string s,t;
-        std:: cout<<validanagram(s,t);
-    }
+    std:: cin>>x;
+    std:: cout<<"x";
 }
