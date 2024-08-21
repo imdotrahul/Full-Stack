a55bf474bc60a5fe8f22ffdbd76b9aa43daddb88
imdotrahul <imdotrahul@gmail.com>
2024-08-21 16:38:49 +0530

    learned execution context

---
+++ Full Stack/shadow.js
@@ -0,0 +1,14 @@
+var name = "Rahul";
+
+function print() {
+  var name = "Shadow";
+  function Name() {
+    var name = "Yadav";
+    console.log(name);
+  }
+  Name();
+  console.log(name);
+}
+print();
+
+console.log(name);
